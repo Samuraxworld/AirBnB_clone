3d610e9b89265d364ea86ab00bbd3b8c3977d6bb
espoupou <espoirativon@gmail.com>
2023-10-15 05:37:00 -0700

    delete diplicate code in reviews

--- models/review.py
+++ models/review.py
@@ -9,15 +9,4 @@ class Review(BaseModel):
 
     place_id = ""
     user_id = ""
-    text = ""#!/usr/bin/python3
-"""This module creates a Review class"""
-
-from models.base_model import BaseModel
-
-
-class Review(BaseModel):
-    """This is the class for managing review objects"""
-
-    place_id = ""
-    user_id = ""
     text = ""
